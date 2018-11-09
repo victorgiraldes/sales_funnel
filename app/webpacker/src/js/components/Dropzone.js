d1142047f6f2fc0bc22d8a80aac6c7877cd3fb77
Nathália Oliveira <nathipsl@hotmail.com>
2018-11-09 21:10:53 -0200

    small fix

--- app/webpacker/src/js/components/Dropzone.js
+++ app/webpacker/src/js/components/Dropzone.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Dropzone = ({ height }) => (
   <div
-    className="rounded-border bg-darken-5 margin-bottom-sm"
+    className="border-rounded bg-darken-10 margin-bottom-sm"
     style={{ height: height }}
   >
   </div>
